bc69513e929ce08fcc8f702e755bd407e65ffc81
FedeSca001 <familadore@gmail.com>
2022-03-03 18:37:09 -0300

    maquetando componentes

--- src/components/HeaderComp/HeaderComp.vue
+++ src/components/HeaderComp/HeaderComp.vue
@@ -1,7 +1,11 @@
 <template>
-  <section>
-    <button type="button">Graphic design</button>
-    <button type="button">Programming</button>
+  <section class="sections">
+    <div class="grafic">
+      <button type="button">Graphic design</button>
+    </div>
+    <div class="progr">
+      <button type="button" class="progr">Programming</button>
+    </div>
   </section>
 </template>
 
@@ -11,6 +15,31 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.grafic{
+  background-image: url("../../assets/images/agv.jpg");
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+div{
+  display: block;
+  width: 50%;
+  align-items: center;
+  text-align: center;
+}
+button{
+  margin: 1vh 1vw;
+  height: 5vh;
+  width: 14vw;
+  font-weight: 800;
+  font-size: 1rem;
+  background: none;
+  border: none;
+}
+.sections{
+  display: flex;
+  background-color: blue;
+}
 </style>
