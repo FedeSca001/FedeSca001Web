e59a5c31f8eab9521a1d87db5d5304ca7af8ba3e
FedeSca001 <familadore@gmail.com>
2022-10-21 19:20:13 +0200

    reform

--- src/components/MainComp/MainComp.vue
+++ src/components/MainComp/MainComp.vue
@@ -1,15 +1,15 @@
 <template>
 <div>
-    <article class="articleComp">
-    <img src="../../assets/images/fotoperfil.jpg" alt="imgperfil" class="imgProfile">
-    <div class="textInit">
-      <h1>Federico Scarpecci</h1>
-      <h3>{{subText}}</h3>
-    </div>
-    </article>
-    <h4>Carta de Presentación</h4>
-    <p class="textPresentacion">
-    Soy desarrollador front-end en búsqueda de mi primer empleo de programador. Comencé mis estudios de el mes de septiembre del año 2021 de forma autodidactica como la mayoría y luego tomé dos cursos, los cuales me ayudaron a consolidar mis conocimientos en las materias estudiadas. En mi camino de aprendizaje llevo visto html, css, javascript, react js y vue js. Se puede ver en mi repositorio de github que subí proyectos usando estas tecnologías, además de librerías como Bootstrap, jquery y Ajax, axios, ReactBootstrap, vuetify. En cuanto a los frameworks, comencé con react js y vue js, pero decidí enfocarme en el segundo. En vue js comencé en su versión 2 y luego estudié su versión 3 con las herramientas setup, pinia,vuex, router y vuetify haciendo llamas API y validando usuarios.
+  <article class="articleComp">
+  <img src="../../assets/images/fotoperfil.jpg" alt="imgperfil" class="imgProfile">
+  <div class="textInit">
+    <h1>Federico Scarpecci</h1>
+    <h3>{{subText}}</h3>
+  </div>
+  </article>
+  <h4>Carta de Presentación</h4>
+  <p class="textPresentacion">
+  Soy desarrollador front-end en búsqueda de mi primer empleo de programador. Comencé mis estudios de el mes de septiembre del año 2021 de forma autodidactica como la mayoría y luego tomé dos cursos, los cuales me ayudaron a consolidar mis conocimientos en las materias estudiadas. En mi camino de aprendizaje llevo visto html, css, javascript, react js y vue js. Se puede ver en mi repositorio de github que subí proyectos usando estas tecnologías, además de librerías como Bootstrap, jquery y Ajax, axios, ReactBootstrap, vuetify. En cuanto a los frameworks, comencé con react js y vue js, pero decidí enfocarme en el segundo. En vue js comencé en su versión 2 y luego estudié su versión 3 con las herramientas setup, pinia,vuex, router y vuetify haciendo llamas API y validando usuarios.
   </p>
 </div>
 </template>
