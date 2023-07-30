1e350a12be71fd18409b43060c052af5366328d5
FedeSca001 <familadore@gmail.com>
2023-07-30 13:06:45 +0200

    datos

--- src/components/InfoPersonal/InfoPersonal.vue
+++ src/components/InfoPersonal/InfoPersonal.vue
@@ -4,27 +4,27 @@
       <h1 class="sectionTitle">Personal Information</h1>
       <ul class="infoList">
         <li>
-          <span class="infoLabel">Nombre:</span>
+          <span class="infoLabel">Nombre: </span>
           <span class="infoValue">Federico Scarpecci</span>
         </li>
         <li>
-          <span class="infoLabel">Edad:</span>
+          <span class="infoLabel">Edad: </span>
           <span class="infoValue">31</span>
         </li>
         <li>
-          <span class="infoLabel">Nacionalidad:</span>
+          <span class="infoLabel">Nacionalidad: </span>
           <span class="infoValue">Argentinian - Italian</span>
         </li>
         <li>
-          <span class="infoLabel">Current Location:</span>
+          <span class="infoLabel">Current Location: </span>
           <span class="infoValue">Spain</span>
         </li>
         <li>
-          <span class="infoLabel">Idiomas:</span>
+          <span class="infoLabel">Idiomas: </span>
           <span class="infoValue">Español - Italiano - Ingles</span>
         </li>
         <li>
-          <span class="infoLabel">Studies:</span>
+          <span class="infoLabel">Studies: </span>
           <span class="infoValue">HTML, CSS, JavaScript, Vue.js, Vuex</span>
         </li>
       </ul>
@@ -33,23 +33,23 @@
       <h1 class="sectionTitle">Contacto</h1>
       <ul class="infoList">
         <li>
-          <span class="infoLabel">Email:</span>
+          <span class="infoLabel">Email: </span>
           <span class="infoValue">familadore@gmail.com</span>
         </li>
         <li>
-          <span class="infoLabel">Teléfono:</span>
+          <span class="infoLabel">Teléfono: </span>
           <span class="infoValue">+34 674296437</span>
         </li>
         <li>
-          <span class="infoLabel">Dirección:</span>
+          <span class="infoLabel">Dirección: </span>
           <span class="infoValue">Palo Mayor 1, Málaga, Andalucía, España</span>
         </li>
         <li>
-          <span class="infoLabel">Instagram:</span>
+          <span class="infoLabel">Instagram: </span>
           <span class="infoValue">instagram.com/fedesca001/</span>
         </li>
         <li>
-          <span class="infoLabel">LinkedIn:</span>
+          <span class="infoLabel">LinkedIn: </span>
           <span class="infoValue">linkedin.com/in/federico-scarpecci-developer/</span>
         </li>
       </ul>
