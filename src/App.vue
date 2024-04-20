36aef3128b02d47ce7b53f3c5ea58c0a94a47002
FedeSca001 <familadore@gmail.com>
2024-04-20 18:56:33 +0200

    img

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   import PortFolio from "./components/PortFolio/PortFolio.vue";
   import FooterComp from "./components/FooterComp/FooterComp.vue";
   import FormContactame from "./components/FormContactame/FormContactame.vue";
-  import CV from "./components/CV/CV.vue"
+
 </script>
 
 <template>
@@ -15,7 +15,6 @@
     <MainComp />
     <PortFolio />
     <ToolsComponent />
-    <CV />
     <InfoPersonal />
     <FormContactame />
     <FooterComp />
